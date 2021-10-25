f13fe21da47e3e0cb841a8a228252d77c1748461
YoramTimmers <89836629+YoramTimmers@users.noreply.github.com>
2021-10-25 11:09:51 +0200

    eerste opdracht

---
+++ opdracht 1 bushcraft.py
@@ -0,0 +1,7 @@
+dieselprijs = 1.67
+afstand = 244 / 12
+afstandvoordisel = 244
+broodjesendrinken =  8.00
+
+prijs = dieselprijs * afstand + broodjesendrinken
+print (f"de prijs van de rit is {prijs * 2}") 
